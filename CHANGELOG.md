604998f9765f2c52256e930c2a8bc34914838b17
Manuel Marquez <buzondemam@gmail.com>
2018-01-08 00:30:43 -0400

    [ADD] brella_groups: user groups for brella

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased]
+### added
+- brella: Module App, installs all dependences for brella.
+- brella_groups: new module with user groups for brella.
---
+++ brella/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Uakami - Manuel Marquez <buzondemam@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Brella',
+    'version': '11.0.0.1.0',
+    'category': 'Sale',
+    'author': 'Uakami',
+    'website': "https://uakami.com/",
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'brella_groups',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
